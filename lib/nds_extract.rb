8d4d0b269ced598f142bb6cb169e76400b1a9a49
ricky wahba <rjwahba@gmail.com>
2020-01-03 21:11:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,11 +8,11 @@ def directors_totals(nds)
   while nds[row_index] do
     sum = 0 
     num_movies=0 
-    while nds[index][:movies][num_movies] do
-      sum += nds[index][:movies][num_movies][:worldwide_gross]
+    while nds[row_index][:movies][num_movies] do
+      sum += nds[row_index][:movies][num_movies][:worldwide_gross]
       num_movies += 1 
     end
-    ret_hash[nds[index][:name]]= sum    
+    movies_money[nds[row_index][:name]]= sum    
     index += 1
   end
   nil
